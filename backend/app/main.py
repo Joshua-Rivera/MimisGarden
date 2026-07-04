68593d299a7fd9cb8811b2a017b534244be3be10
Joshua-Rivera <joshua.rivera46@upr.edu>
2026-07-04 14:31:12 -0700

    Main file completed, instance of FastAPI app, should be working, main.py is completed.

---
+++ backend/app/main.py
@@ -0,0 +1,17 @@
+#import FastAPI from the fastAPI library
+from fastapi import FastAPI
+#check backend is running well and posting
+from app.api.v1.routes_health import router as health_router
+#routes images for prediction
+from app.api.v1.routes_prediction import router as prediction_router
+
+#create FastAPI instance
+
+app = FastAPI(
+    title = "Mimi's Plants",
+    description = "This is a plant disease detection API that uses a deep learning model to predict the disease of a plant based on an image of its leaves.",
+    version = "1.0.0",
+    )
+#connect health state to app and connect predictions to app
+app.include_router(health_router)
+app.include_router(prediction_router)
