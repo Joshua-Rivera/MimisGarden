7ee3741e9df342cccd6943c33a8fbaa87bebbb85
Joshua-Rivera <joshua.rivera46@upr.edu>
2026-07-05 12:30:46 -0700

    documented save image file

--- backend/app/services/image_storage_service.py
+++ backend/app/services/image_storage_service.py
@@ -1,15 +1,15 @@
-
+#importas path from the patthlib file for easy usage instead of just stringss :D
 from pathlib import Path
-
+#creates a random unique id for each image 
 from uuid import uuid4
-
+#imports the UploadFile class from FastAPI to handle file uploads in the API endpoints
 from fastapi import UploadFile
 
-
-UPLOAD_DIR = Path ("storage/uploaded_images")
-
+#creates folder path for where the uploaded images will reside
+UPLOAD_DIR = Path("storage/uploaded_images")
+#creates the uploads folder if it does not exist already, allowing for nested directories to be created as needed
 UPLOAD_DIR.mkdir(parents=True, exist_ok=True)
-
+#specifies allowed file types 
 ALLOWED_EXTENSIONS = {".jpg", ".jpeg", ".png", ".webp"}
 
 
