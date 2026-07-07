169fc76c451419f81b7282ac667f963d1bfe9ad3
Joshua-Rivera <joshua.rivera46@upr.edu>
2026-07-06 20:06:27 -0700

    Updated image storage service to work along size the image validation service

--- backend/app/services/image_storage_service.py
+++ backend/app/services/image_storage_service.py
@@ -5,6 +5,7 @@ from uuid import uuid4
 #imports the UploadFile class from FastAPI to handle file uploads in the API endpoints
 from fastapi import UploadFile
 
+from app.services.image_validation_service import validate_uploaded_image
 #creates folder path for where the uploaded images will reside
 UPLOAD_DIR = Path("storage/uploaded_images")
 #creates the uploads folder if it does not exist already, allowing for nested directories to be created as needed
@@ -23,20 +24,13 @@ async def save_uploaded_image(file: UploadFile) -> str:
     Returns:
         str: The path to the saved image.
     """
-    original_filename = file.filename or ""
-
-    file_extension = Path(original_filename).suffix.lower()
-
-    if file_extension not in ALLOWED_EXTENSIONS:
-        file_extension = ".jpg"  # Default to .jpg if the extension is not allowed
-    
-    image_id = f"img_{uuid4().hex}"
-
+    # Validate the uploaded image using the image validation service
+    contents, file_extension = await validate_uploaded_image(file)
+    # generate a unique image ID using uuid4 and append the file extension to create a unique filename for the uploaded image
+    image_id = f"img_{uuid4().hex}{file_extension}"
+    # create the full path for the image to be saved, combining the upload directory, unique image ID, and file extension
     image_path = UPLOAD_DIR / f"{image_id}{file_extension}"
-
-    contents = await file.read()
-
+    # write the contents of the uploaded image to the specified path
     with open(image_path, "wb") as image_file:
         image_file.write(contents)
-
-    return str(image_path)
+    return str(image_path)  # Return the path to the saved image
