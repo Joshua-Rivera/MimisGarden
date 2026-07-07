787d253da00386b3c5c9ca4a2759c41621b6ee8e
Joshua-Rivera <joshua.rivera46@upr.edu>
2026-07-06 19:55:18 -0700

    Updated route endpoints to include past end point predictions and log file uploads onto the db session

--- backend/app/api/v1/routes_predictions.py
+++ backend/app/api/v1/routes_predictions.py
@@ -3,6 +3,8 @@ from fastapi import APIRouter, File, UploadFile
 # import prediction responses and created predictions for endpoint functionality
 from app.schemas.prediction_schema import PredictionResponse
 from app.services.prediction_service import create_prediction
+# import get_db from the session module in the db package to manage database sessions
+from app.db.session import get_db
 # Routes predictions through the API endpoints, allowing users to upload images and receive predictions about plant health.
 router = APIRouter(prefix="/api/v1", tags=["predictions"])
 
@@ -16,4 +18,17 @@ async def predict_plant_health(file: UploadFile = File(...)):
     Args:
         file (UploadFile): The uploaded image file.
     """
+    file: UploadFile = File(...)
+    db: Session = Depends(get_db)
     return await create_prediction(file)
+
+@router.get("/predictions")
+def get_predictions(db: Session = Depends(get_db)):
+    """
+    Endpoint to retrieve all predictions from the database.
+
+    Args:
+        db (Session): The database session.
+    """
+    predictions = db.query(PredictionLog).order_by(PredictionLog.created_at.desc()).all()()
+    return predictions
