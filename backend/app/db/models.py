58cb4cbb361a8d7aedfc35da17451cfbd8459371
Joshua-Rivera <joshua.rivera46@upr.edu>
2026-07-08 11:28:01 -0700

    Added two imports and review class for formatting of human feedback for continous ease of betterment of the model

--- backend/app/db/models.py
+++ backend/app/db/models.py
@@ -1,8 +1,9 @@
 # current date and time
 from datetime import datetime
+from typing import Text
 # SQLAlchemy imports for defining database models
 from sqlalchemy import Boolean, Column, DateTime, Float, String
-
+from sqlalchemy.orm import relationship
 from app.db.session import Base  # Import the Base class from the session module
 
 class PredictionLog(Base):
@@ -22,4 +23,21 @@ class PredictionLog(Base):
     model_version = Column(String, nullable=False)
     needs_review = Column(Boolean, nullable=False)
 
-    created_at = Column(DateTime, default=datetime.utcnow)
+    created_at = Column(DateTime, default=datetime.utcnow, nullable=False)  # Timestamp for when the prediction was logged
+
+class Review(Base):
+    """
+    Database model for storing reviews of predictions made by the plant disease detection API.
+    This model allows users to provide feedback on the accuracy and usefulness of the predictions.
+    """
+    __tablename__ = "reviews"  # Name of the table in the database
+
+    review_id = Column(String, primary_key=True, index=True)
+    prediction_id = Column(String, 
+                           ForeginKey("prediction_logs.prediction_id"),
+                           nullable = False,
+                           index = False) # Foreign key to PredictionLog (not enforced here)
+    correct_label = Column(String, nullable=False)
+    review_notes = Column(Text, nullable=True)
+    reviewed_at = Column(DateTime, default=datetime.utcnow, nullable=False)  # Timestamp for when the review was submitted
+    prediction = relationship("PredictionLog")
