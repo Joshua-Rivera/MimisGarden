c785efff9a4e283bb3cb2b1d3e550596a02a1d16
Joshua-Rivera <joshua.rivera46@upr.edu>
2026-07-15 14:35:38 -0400

    Completed a limitations section which details what the model can and can't do

---
+++ frontend/src/components/LimitationsSection.jsx
@@ -0,0 +1,16 @@
+export default function LimitationsSection() {
+
+    return (
+        <section className="limitations-section">
+            {/*small section label*/}
+            <p className="small-title">Limitations</p>
+            {/*main title*/}
+            <h2>Guidance, not a final diagnosis</h2>
+            <p>Mimi&apos;s Garden provides assistance in the state of the plant through predictions based
+                on user uploaded images. Confidence scores do not guarantee accuracy. Lighting, image quality,
+                leaf angle, background, and plant type may affect the accuracy of the predictions.
+                </p>
+        </section>
+    );
+
+}
