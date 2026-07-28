5f9929cc5b878b95a3d661b07a8e7f3613b50949
Joshua-Rivera <joshua.rivera46@upr.edu>
2026-07-28 18:16:42 -0400

    Added getter to main.py, created frontend containerization.

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,29 +1,38 @@
-#import FastAPI from the fastAPI library
+# import FastAPI from the fastAPI library
 from fastapi import FastAPI
-#check backend is running well and posting
+
+# check backend is running well and posting
 from app.api.v1.routes_health import router as health_router
-#routes images for prediction
+
+# routes images for prediction
 from app.api.v1.routes_predictions import router as prediction_router
+
 # import Base and engine from the session module in the db package
 from app.db.session import Base, engine
+
 # import models from the models module in the db package
 from app.db import models
+
 # import models router from the routes_model module in the api.v1 package
 from app.api.v1.routes_model import router as models_router
+
 # import models router from the routes_model module in the api.v1 package
 from app.api.v1.routes_reviews import router as review_router
+
 # import models router from the routes_model module in the api.v1 package
 from app.api.v1.routes_metrics import router as metrics_router
+
 # import CORSMiddleware from the fastAPI.middleware.cors module
 from fastapi.middleware.cors import CORSMiddleware
+
 Base.metadata.create_all(bind=engine)  # Create the database tables if they don't exist
-#create FastAPI instance
+# create FastAPI instance
 app = FastAPI(
-    title = "Mimi's Plants",
-    description = "This is a plant disease detection API that uses a deep learning model to predict the disease of a plant based on an image of its leaves.",
-    version = "1.0.0",
-    )
-#connect routers to app
+    title="Mimi's Plants",
+    description="This is a plant disease detection API that uses a deep learning model to predict the disease of a plant based on an image of its leaves.",
+    version="1.0.0",
+)
+# connect routers to app
 app.include_router(health_router)
 app.include_router(prediction_router)
 app.include_router(review_router)
@@ -31,7 +40,7 @@ app.include_router(models_router)
 app.include_router(metrics_router)
 # use uvicorn to run the app with the command: python -m uvicorn app.main:app --reload
 # use the following command to get into the correct directory:
-    # cd cd C:\Users\{your user}\MimisGarden\backend\app (windows)
+# cd cd C:\Users\{your user}\MimisGarden\backend\app (windows)
 
 
 app.add_middleware(
@@ -44,3 +53,9 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
+# get request for favicon verifying that the backend is running well and posting
+@app.get("/favicon.ico")
+async def root():
+    return {"message": "Mimi is gardening"}
