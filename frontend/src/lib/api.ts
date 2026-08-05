bacf1d41a7a54019677940c8e2563e364cffc835
Joshua-Rivera <joshua.rivera46@upr.edu>
2026-08-05 14:29:05 -0400

    Updated paths for streamlining deployment due to bad pathing and envs

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,7 +1,9 @@
 // backend URL
 const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL ?? "http://127.0.0.1:8000";
-// main API path
+  import.meta.env.VITE_API_BASE_URL ||
+  (import.meta.env.DEV
+    ? "http://127.0.0.1:8000"
+    : "https://mimisgarden.onrender.com"); // main API path
 const API_V1 = `${API_BASE_URL}/api/v1`;
 // sends an image to the backend for processing
 export async function createPrediction(file: File) {
