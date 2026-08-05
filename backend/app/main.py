2326bd2fda433715e830b90bb55c1276a12e8edd
Joshua-Rivera <joshua.rivera46@upr.edu>
2026-08-05 02:05:37 -0400

    Added vite environment, and updated allowed urls for requests

--- backend/app/main.py
+++ backend/app/main.py
@@ -48,6 +48,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://localhost:5500",
+        "https://mimis-garden.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
